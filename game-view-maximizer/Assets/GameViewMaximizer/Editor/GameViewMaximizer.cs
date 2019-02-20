b022a330cd600e2bbafad86283ca9d611683063d
tomori_aki <tomori.aki2961@gmail.com>
2019-02-20 23:04:25 +0900

    Unityプロジェクトの追加

---
+++ game-view-maximizer/Assets/GameViewMaximizer/Editor/GameViewMaximizer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using System;
+
+namespace SIN.Editor
+{
+    public class GameViewMaximizer
+    {
+        [MenuItem("Window/Maximize GameView %#x")]
+        private static void MaximizeGameView()
+        {
+            var gameViewWindowType = Type.GetType("UnityEditor.GameView,UnityEditor");
+            var window = EditorWindow.GetWindow(gameViewWindowType);
+            window.maximized = !window.maximized;
+        }
+    }
+}
